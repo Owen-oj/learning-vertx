594751e02c09068ff3d0d83ab969fda54b92c1aa
Owen Jubilant <jubifis@gmail.com>
2020-03-10 14:58:56 +0000

    implemented Log4j 2 logging utility and small refactoring

--- src/main/java/com/example/owen/books/MainVerticle.java
+++ src/main/java/com/example/owen/books/MainVerticle.java
@@ -9,8 +9,11 @@ import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
   private InMemoryBookStore store = new InMemoryBookStore();
 
   @Override
@@ -20,55 +23,34 @@ public class MainVerticle extends AbstractVerticle {
     Router books = Router.router(vertx);
     books.route().handler(BodyHandler.create());
 
+    //todo extract endpoints to separate controller
     //GET /books
-    books.get("/books").handler(req -> {
-      System.out.println("Printing list of books");
-      req.response()
-        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-        .end(store.getAll().encode());
-    });
-
+    all(books);
     //POST /books
-    books.post("/books").handler(req -> {
-      //get the body
-      final JsonObject requestBody = req.getBodyAsJson();
-      //store book in memory
-      store.add(requestBody.mapTo(Book.class));
-      req.response()
-        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-        .setStatusCode(HttpResponseStatus.CREATED.code())
-        .end(requestBody.encode());
-
-    });
-
+    store(books);
     //PUT /books/:isbn
-    books.put("/books/:isbn").handler(req -> {
-      final String isbn = req.pathParam("isbn");
-      final JsonObject requestBody = req.getBodyAsJson();
-      final Book updatedBook = store.update(isbn, requestBody.mapTo(Book.class));
-
-      req.response()
-        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-        .end(JsonObject.mapFrom(updatedBook).encode());
-    });
-
+    update(books);
     //GET /books/:isbn
-    books.get("/books/:isbn").handler(req -> {
-      final String isbn = req.pathParam("isbn");
-      final Book book = store.get(isbn);
-      if (book == null) {
-        req.response()
-          .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-          .setStatusCode(HttpResponseStatus.NOT_FOUND.code())
-          .end(new JsonObject().put("message", "ISBN does not match any book in the database").encode());
-      } else {
-        req.response()
-          .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-          .end(JsonObject.mapFrom(book).encode());
-      }
-    });
-
+    show(books);
     //DELETE /books/:isbn
+    destroy(books);
+    //Handle errors
+    handleErrors(books);
+
+
+    startServer(startFuture, books);
+  }
+
+  private void handleErrors(Router books) {
+    books.errorHandler(500, event -> {
+      LOG.error("Failure", event.failure());
+      event.response()
+        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+        .end(new JsonObject().put("error", event.failure().getMessage()).encode());
+    });
+  }
+
+  private void destroy(Router books) {
     books.delete("/books/:isbn").handler(req -> {
       final String isbn = req.pathParam("isbn");
       final Book deletedBook = store.destroy(isbn);
@@ -83,24 +65,66 @@ public class MainVerticle extends AbstractVerticle {
           .end(new JsonObject().put("message", "Book Deleted Successfully").encode());
       }
     });
+  }
 
-    //Handle errors
-    books.errorHandler(500, event -> {
-      System.err.println("Failure: " + event.failure());
-      event.response()
-        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-        .end(new JsonObject().put("error", event.failure().getMessage()).encode());
+  private void show(Router books) {
+    books.get("/books/:isbn").handler(req -> {
+      final String isbn = req.pathParam("isbn");
+      final Book book = store.get(isbn);
+      if (book == null) {
+        req.response()
+          .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+          .setStatusCode(HttpResponseStatus.NOT_FOUND.code())
+          .end(new JsonObject().put("message", "ISBN does not match any book in the database").encode());
+      } else {
+        req.response()
+          .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+          .end(JsonObject.mapFrom(book).encode());
+      }
     });
+  }
 
+  private void update(Router books) {
+    books.put("/books/:isbn").handler(req -> {
+      final String isbn = req.pathParam("isbn");
+      final JsonObject requestBody = req.getBodyAsJson();
+      final Book updatedBook = store.update(isbn, requestBody.mapTo(Book.class));
 
-    startServer(startFuture, books);
+      req.response()
+        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+        .end(JsonObject.mapFrom(updatedBook).encode());
+    });
+  }
+
+  private void store(Router books) {
+    books.post("/books").handler(req -> {
+      //get the body
+      final JsonObject requestBody = req.getBodyAsJson();
+      //store book in memory
+      store.add(requestBody.mapTo(Book.class));
+      req.response()
+        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+        .setStatusCode(HttpResponseStatus.CREATED.code())
+        .end(requestBody.encode());
+
+    });
+  }
+
+  private void all(Router books) {
+    books.get("/books").handler(req -> {
+      LOG.info("Printing list of books");
+      req.response()
+        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+        .end(store.getAll().encode());
+    });
   }
 
   private void startServer(Future<Void> startFuture, Router books) {
+    LOG.info("Booting up server....");
     vertx.createHttpServer().requestHandler(books).listen(8888, http -> {
       if (http.succeeded()) {
         startFuture.complete();
-        System.out.println("HTTP server started on port 8888");
+        LOG.info("HTTP server started on port 8888");
       } else {
         System.out.println(http.cause().getMessage());
         startFuture.fail(http.cause());
